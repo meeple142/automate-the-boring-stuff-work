94987f96c1b02bef9bc50c805db985c193231b12
Joshua McKinney <mckinneyj@byui.edu>
2017-04-04 00:58:45 -0600

    did it two differnt ways

--- lesson4/listToString.py
+++ lesson4/listToString.py
@@ -7,6 +7,20 @@ def listToString(listIn):
     strOut += 'and ' + str(listIn[-1])
     return strOut
     
+def listToPrintString(listIn):
+    for i in range(len(listIn)-1):
+        print(str(listIn[i]), end=', ')
+    
+    print("and " + str(listIn[-1]))
+    
+        
+def listToPrintString2(listIn):
+    for i in range(len(listIn)):
+        if i != (len(listIn) -1):
+            print(str(listIn[i]), end=', ')
+        else:
+            print("and " + str(listIn[i]))
+    
 #the lists I'm gunna use    
 bestList = [1,3,3,3,3,3,3,2,3,4,5]
 spam = ['apples', 'bananas', 'tofu', 'cats']
@@ -16,5 +30,12 @@ bestListStr = listToString(bestList)
 spamStr = listToString(spam)
 
 #print them 
+print('My Way:')
 print(bestListStr)
 print(spamStr)
+print('\nYour Way:')
+listToPrintString(bestList)
+listToPrintString(spam)
+print('\nIf Way:')
+listToPrintString2(bestList)
+listToPrintString2(spam)
